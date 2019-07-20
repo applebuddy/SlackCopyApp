afabab996d44f6a843e25fec5072714e8498c1d7
applebuddy <applebuddy@nate.com>
2019-07-20 14:07:00 +0900

    Setting Gradient LayerView and Some UI

---
+++ GradientView.swift
@@ -0,0 +1,36 @@
+//
+//  GradientView.swift
+//  SmackPractice
+//
+//  Created by Min Kyeong Tae on 20/07/2019.
+//  Copyright © 2019 Min Kyeong Tae. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.287455678, green: 0.3025078475, blue: 0.8791555762, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+//          self.setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor =  #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
