feb43658006d34819bd14eaed12c23bdd98421db
MinKyeongTae <kyeongtea92@gmail.com>
2019-07-30 23:12:27 +0900

    Add AvatarBGColor, ActivityIndicator Function

--- Constants.swift
+++ Constants.swift
@@ -13,22 +13,34 @@ typealias Jonny = String
 
 let name: Jonny = "Jonny"
 
-// URL Constants
+// MARK: - URL Constants
+
 let BASE_URL = "https://slackcopyapp3.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
 
-// Segues
+// MARK: - Colors
+
+let smackPurplePlaceholder = #colorLiteral(red: 0.5143675086, green: 0.3790132705, blue: 1, alpha: 0.5)
+
+// MARK: - Notification
+
+let NOTIFI_USER_DATA_DID_CHANGE = Notification.Name("notiUserDataChanged")
+
+// MARK: - Segues
+
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
 let To_AVATAR_PICKER = "toAvatarPicker"
 
-// User Defaults
+// MARK: - User Defaults
+
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
-// Headers
+// MARK: - Headers
+
 let HEADER = ["Content-Type": "application/json; charset=utf-8"]
